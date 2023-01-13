1fe4761c405aaae4acb73a9dd5f9f0bf854e017e
maggymbur <mburumaggy62@gmail.com>
2023-01-13 13:23:43 +0300

    my first commit

---
+++ tutorial.php
@@ -0,0 +1,72 @@
+
+<?php
+/*$str = "hello world"; // declairing variables
+$num = 5;
+$float =7.6;
+
+echo '<h1>' . $str . '<h1>'; // printing variables
+echo '<h2>' . $num . '<h2>';
+echo '<h3>' . $float . '<h3>';
+
+$laptop_brand = array("hp", "lenovo", "asus", "dell");
+echo ($laptop_brand[1]); // how to print an array.
+echo var_dump($laptop_brand[1]);// another way of printing an array.
+
+
+define('laptops', 'hp, lenovo, asus');//constant
+echo laptops;
+
+$var = 5;
+if($var > 7){
+    echo $var;
+}
+
+else{
+    echo "the condition did not meet";
+}
+
+$var = 6;
+if($var % 2 == 0){
+    echo "divisible by 2";
+}
+else {
+    echo "no is odd";
+}
+
+$level = 3;
+switch($level){
+    case 1: 
+        echo "You are playing an easy level.";
+    break; 
+    
+    case 2: 
+        echo "You are playing a medium level.";
+    break;    
+
+    case 3: 
+        echo "You are playing a hard level.";
+    break; 
+    
+    case 4: 
+        echo "You are playing an exteme level.";
+    break;  
+    default:
+         echo "invalid input";
+      
+}*/
+
+function name(){
+    for ($var = 1; $var =>6; $var++;)
+    echo "number: $var <br>";
+}
+
+}
+name();
+
+echo "I'm going to run it again";
+
+name();
+
+?>
+
+
